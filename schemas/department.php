247a5fc7beb7854760791c4e6451b80305cd9db1
Benjamin LeBrun <bl.lebrun@gmail.com>
2019-12-08 19:21:29 -0700

    cleanup, departments

--- schemas/department.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $new_user = array(
-        "prefix"            => $_POST['prefix'],
-        "department_head"   => $_POST['department_head'],
-        "title"             => $_POST['title'],
-        "description"       => $_POST['description']
-    );
-?>
--- schemas/professor.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $new_professor = array(
-        "ID"            => $_POST['ID'],
-        "name"          => $_POST['name'],
-        "department"    => $_POST['department'],
-        "phone"         => $_POST['phone'],
-        "email"         => $_POST['email']
-    );
-?>
