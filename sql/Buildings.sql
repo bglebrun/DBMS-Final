e09122b01f0643f11018d844df723336db05f03c
7131322 <Benjamin.LeBrun@mines.sdsmt.edu>
2019-12-09 17:41:18 -0700

    Singular population file

--- sql/Buildings.sql
+++
@@ -1,7 +0,0 @@
-use db_s7131322_f19;
-
-insert into Buildings (BuildingName) values ('Colbert');
-insert into Buildings (BuildingName) values ('Snow');
-insert into Buildings (BuildingName) values ('Yannikov');
-insert into Buildings (BuildingName) values ('Bellenie');
-insert into Buildings (BuildingName) values ('Trevaskis');
--- sql/Prefixes.sql
+++
@@ -1,5 +0,0 @@
-insert into Prefixes (Prefix) values ('Skyble');
-insert into Prefixes (Prefix) values ('Kamba');
-insert into Prefixes (Prefix) values ('Devcast');
-insert into Prefixes (Prefix) values ('Kazio');
-insert into Prefixes (Prefix) values ('Shuffledrive');
